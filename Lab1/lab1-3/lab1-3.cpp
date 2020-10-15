167d526ecb2b9d0f1999c15d586b77518ef72652
Nikita <63471940+Daelon02@users.noreply.github.com>
2020-10-15 16:30:58 +0300

    Добавление Lab2

---
+++ Lab1/lab1-3/lab1-3.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include<windows.h>
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	float a, b, c, d;
+	printf("Введите число:\n");
+	scanf_s("%f", &a);
+	b = a * a;
+	c = b * b;
+	d = c * c;
+	printf("Число в 8-ой степени: d=%f\n", d);
+	return 0;
+}
---
+++ Lab1/lab1-4/lab1-4.cpp
@@ -0,0 +1,14 @@
+﻿#include<cstdio>
+#include<windows.h>
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int a = 2;
+	int b = 6;
+	printf("Числа не изменённые: %d, %d \n", a, b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("Числа изменились: %d, %d \n", a, b);
+}
