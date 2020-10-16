13e2fc7c92beef46a8a2b67ea3bba827b0c98187
Nikita <63471940+Daelon02@users.noreply.github.com>
2020-10-16 15:21:35 +0300

    Изменил переменные в Lab2

--- Lab2/Lab2/Lab2.cpp
+++ Lab2/Lab2/Lab2.cpp
@@ -1,26 +1,35 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 #include<Windows.h>
 
 int main(void) {
-	SetConsoleOutputCP(1251);
-	SetConsoleCP(1251);
-	char name1[9], name2[9], name3[9];
-	char sc1, sc2, sc3;
-	unsigned short cnt1, cnt2, cnt3;
-	double sq1, sq2, sq3;
-	printf("1. Введите: вещество, тип, влажность, коэффициент: ");
-	scanf_s("%s %c %d %f", &name1, &sc1, &cnt1, &sq1);
-	printf("2. Введите: вещество, тип, влажность, коэффициент: ");
-	scanf_s("%s %c %d %f", &name2, &sc2, &cnt2, &sq2);
-	printf("3. Введите: вещество, тип, влажность, коэффициент: ");
-	scanf_s("%s %c %d %f", &name3, &sc3, &cnt3, &sq3);
-	printf("|---------------------------------------------|\n");
-	printf("|  Коэффициенты теплопроводимости материаллов | |\n");
-	printf("|---------------------------------------------|\n");
-	printf("| Вещество | тип | влажность |  коэффициент   |\n");
-	printf("|----------|-------|-----------|--------------|\n");
-	printf("| %9s | %c | %3d | %-5.1f |\n", name1, sc1, cnt1, sq1);
-	printf("| %9s | %c | %3d | %-5.1f |\n", name2, sc2, cnt2, sq2);
-	printf("| %9s | %c | %3d | %-5.1f |\n", name3, sc3, cnt3, sq3);
-	printf("|---------------------------------------------|\n");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+
+    char sc1, sc2, sc3;
+    double quan1, quan2, quan3;
+    long double cnt1, cnt2, cnt3;
+    long long int width1, width2, width3;
+
+
+    printf("1. Введите: наименование, тип, количество, вес 1 детали(г): ");
+    scanf_s("%c %lf %Lf %lli", &sc1, &quan1, &cnt1, &width1);
+
+    printf("2. Введите: наименование, тип, количество, вес 1 детали(г): ");
+    scanf_s("%c %lf %Lf %lli", &sc2, &quan2, &cnt2, &width2);
+
+    printf("3. Введите: наименование, тип, количество, вес 1 детали(г): ");
+    scanf_s("%c %lf %Lf %lli", &sc3, &quan3, &cnt3, &width3);
+
+    printf("|---------------------------------------------|\n");
+    printf("|  Ведомость деталей | |\n");
+    printf("|---------------------------------------------|\n");
+    printf("| Наименование | Тип | Количество |  Вес 1 детали (г)   |\n");
+    printf("|----------|-------|-----------|--------------|\n");
+    printf("| %c | %lf | %Lf | %lli |\n", sc1, quan1, cnt1, width1);
+    printf("| %c | %lf | %Lf | %lli |\n", sc2, quan2, cnt2, width2);
+    printf("| %c | %lf | %Lf | %lli |\n", sc3, quan3, cnt3, width3);
+    printf("|---------------------------------------------|\n");
+    return 0;
 }
