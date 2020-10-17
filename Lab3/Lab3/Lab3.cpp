8994ca8ad77a413fc127392eb69d74680a5f71cc
Nikita <63471940+Daelon02@users.noreply.github.com>
2020-10-17 11:59:47 +0300

    Добавлена и полностью работает Lab3

--- Lab3/Lab3/Lab3.cpp
+++ Lab3/Lab3/Lab3.cpp
@@ -1,24 +1,16 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<math.h>
+#include<cmath>
 #include<windows.h>
 
 using namespace std;
 int main(void) {
 	system("chcp 1251");
 	system("cls");
-	int a, c;
-	a = 4;
-	double b = 3, d = 2, result;
-	result = pow(a - b, d);
-	double x, y, ax;
-	double t1, t2;
-	c = 1;
-	d = 2;
-	b = 6;
+	float t1, t2, a = 1, c = 6, b, d = 4, x, y, ax;
 	printf("Enter x, y >");
-	scanf("%lf %lf", &x, &y);
-	t1 = b/(a - b) * (b = x) - a/(a - b) * result * log10( a + x/b + x);
+	scanf("%f %f", &x, &y);
+	t1 = b/(a - b) * (b = x) - a/pow((a - b), 2) * log10( a + x/b + x);
 	ax = a * x;
 	t2 = c/a * (log10( tan( ax/d) - c/sin( ax)));
 	printf("t1 = %lg\n", t1);
