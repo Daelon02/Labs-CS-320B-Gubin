cf087bf93b2be39654099d0755e6508b5b20eac4
Nikita <63471940+Daelon02@users.noreply.github.com>
2020-10-18 12:24:38 +0300

    Завершена работа над Lab4

---
+++ Lab4-1/Lab4-1.cpp
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+#include<windows.h>
+
+int main() {
+    system("chcp 1251");
+    system("cls");
+    printf("Имеем части структуры:  1 1 1 0 0 0 1 D B B B B A A A A \n");
+    int result = 0;
+    result |= 0xA << 0;
+    printf("Cместим значения поэтапно: 0xA<<\t%d\n", result);
+    result |= 0xA << 1;
+    printf("Cместим значения поэтапно: 0xA<<\t%d\n", result);
+    result |= 0xA << 2;
+    printf("Cместим значения поэтапно: 0xA<<\t%d\n", result);
+    result |= 0xA << 3;
+    printf("Cместим значения поэтапно: 0xA<<\t%d\n", result);
+    result |= 0xB << 4;
+    printf("Cместим значения поэтапно: 0xB<<\t%d\n", result);
+    result |= 0xB << 5;
+    printf("Cместим значения поэтапно: 0xB<<\t%d\n", result);
+    result |= 0xB << 6;
+    printf("Cместим значения поэтапно: 0xB<<\t%d\n", result);
+    result |= 0xB << 7;
+    printf("Cместим значения поэтапно: 0xB<<\t%d\n", result);
+    result |= 0xD << 8;
+    printf("Cместим значения поэтапно: 0xD<<\t%d\n", result);
+    result |= 0x1 << 9;
+    printf("Cместим значения поэтапно: 0x1<<\t%d\n", result);
+    result |= 0x0 << 10;
+    printf("Cместим значения поэтапно: 0x0<<\t%d\n", result);
+    result |= 0x0 << 11;
+    printf("Cместим значения поэтапно: 0x0<<\t%d\n", result);
+    result |= 0x0 << 12;
+    printf("Cместим значения поэтапно: 0x0<<\t%d\n", result);
+    result |= 0x1 << 13;
+    printf("Cместим значения поэтапно: 0x1<<\t%d\n", result);
+    result |= 0x1 << 14;
+    printf("Cместим значения поэтапно: 0x1<<\t%d\n", result);
+    result |= 0x1 << 15;
+    printf("Cместим значения поэтапно: 0x1<<\t%d\n\n", result);
+
+    printf("Получено число: %d", result);
+
+    return 0;
+}
---
+++ Lab4/Lab4/Lab4.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include<windows.h>
+
+int main() {
+    system("chcp 1251");
+    system("cls");
+    printf("*** Программа конвертирует значения в 1-битовое число ***\n");
+    int right = 0, result;
+    int a, b, c, d, e, f, g, h;
+    do {
+        printf("Введите 8 челочисленных значений [ 0 или 1]\n");
+        printf("для формирования 1-бaйтового типа: ");
+
+        scanf_s("%d %d %d %d %d %d %d %d", &a, &b, &c, &d, &e, &f, &g, &h);
+        if ((a < 0 || a > 1) || (b < 0 || b > 1) || (c < 0 || c > 1) ||
+            (d < 0 || d > 1) || (e < 0 || e > 1) || (f < 0 || f > 1) ||
+            (g < 0 || g > 1) || (h < 0 || h > 1)) {
+            printf("Значения не должны быть 1 или 0\n\n");
+        }
+        else right = 1;
+    } while (!right);
+
+    printf("Получены значения: %d %d %d %d %d %d %d %d\n",
+        h, g, f, e, d, c, b, a);
+    printf("\n*** Cмещение начинается с самого крайнего правого значения***\n\n");
+    result = 0;
+    printf("До смещения результат равен: %d\n", result);
+    result |= a << 0;
+    result |= b << 1;
+    result |= c << 2;
+    result |= d << 3;
+    result |= e << 4;
+    result |= f << 5;
+    result |= g << 6;
+    result |= h << 7;
+    printf("После смещения результат равен: %d\n", result);
+
+    return 0;
+}
