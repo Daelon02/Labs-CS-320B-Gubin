2a8ea9cb985edddeb1a997425f9f7fda2cec4daa
Nikita <63471940+Daelon02@users.noreply.github.com>
2020-12-17 23:59:35 +0200

    Added RGZ. 2 types of sort

---
+++ RGZ/RGZ/RGZ.cpp
@@ -0,0 +1,69 @@
+﻿#include<iostream>
+#include<ctime>
+#include<chrono>
+#include<algorithm>
+#define K 10
+#define N 1
+using namespace std;
+double aver_dur, dur_arr[N + 10];
+void shell_sort(int* a, int n) {
+    int h, i, j, t;
+    for (h = n; h /= 2;) {
+        for (i = h; i < n; i++) {
+            t = a[i];
+            for (j = i; j >= h && t < a[j - h]; j -= h) {
+                a[j] = a[j - h];
+            }
+            a[j] = t;
+        }
+    }
+}
+
+int main() {
+    int A[K], A_copy[K];
+    srand(time(0));
+    for (int i = 0; i < K; ++i) {
+        A[i] = rand() % 100;
+        A_copy[i] = A[i];
+    }
+    cout << "Original array:" << endl;
+    for (int i = 0; i < K; ++i)
+        printf("%d ", A[i]);
+    cout << endl << "Sorted array: " << endl;
+    shell_sort(A, K);
+    for (int i = 0; i < K; ++i)
+        cout << A[i] << " ";
+    /*
+     for (int j = 0; j < N; ++j) {
+         for (int l = 0; l < K; ++l)
+             A[l] = A_copy[l];
+         printf("\n Sorted array: %d", A[i]);
+         shell_sort(A, K);
+         for (int i = 0; i < K; ++i)
+             cout << A[i] << " ";
+     }*/
+    for (int k = 0; k < N; ++k) {
+        // Counting duration of sort
+
+        // Seeking the time
+        auto start = chrono::high_resolution_clock::now();
+        shell_sort(A, K);
+
+        // Seeking the time again 
+        auto end = chrono::high_resolution_clock::now();
+
+        // Counting period of time
+        chrono::duration<double> duration = end - start;
+
+        // Filling in duration array
+        dur_arr[k] = duration.count();
+        for (int i = 0; i < k; ++i)
+            A[i] = A_copy[i];
+
+        printf("\n Duration: %.7f\n", dur_arr[k]);
+        sort(dur_arr, dur_arr + N);
+        for (int i = 100; i < N - 100; ++i)
+            aver_dur += dur_arr[i];
+        cout << "Average duration = " << aver_dur / (N - 201) << "s";
+    }
+}
---
+++ RGZ/Shuttle sort/Shuttle sort.cpp
@@ -0,0 +1,81 @@
+﻿#include<iostream>
+#include<ctime>
+#include<chrono>
+#include<algorithm>
+#define K 10
+#define N 1
+using namespace std;
+
+//ф-ция для обмена значений ячеек
+void swapEl(int* arr, int i)
+{
+    int buff;
+    buff = arr[i];
+    arr[i] = arr[i - 1];
+    arr[i - 1] = buff;
+}
+
+//ф-ция челночной-сортировки
+void ShutterSort(int* arr, int size)
+{
+    int leftMark = 1;
+    int rightMark = size - 1;
+    while (leftMark <= rightMark)
+    {
+        for (int i = rightMark; i >= leftMark; i--)
+            if (arr[i - 1] > arr[i]) swapEl(arr, i);
+        leftMark++;
+
+
+        for (int i = leftMark; i <= rightMark; i++)
+            if (arr[i - 1] > arr[i]) swapEl(arr, i);
+        rightMark--;
+
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    double aver_dur = 0, dur_arr[N + 10];
+    int size = rand() % 50;
+    int* A = new int[size];
+
+    for (int k = 0; k < size; k++)
+    {
+        A[k] = size - k; // запись значений по убыванию
+        cout << A[k] << " ";
+    }
+
+    ShutterSort(A, size); // сортировка
+
+    cout << "\nМассив после сортировки:\n";
+    for (int k = 0; k < size; k++)
+    {
+        cout << A[k] << " ";
+    }
+    cout << endl;
+    for (int k = 0; k < N; ++k) {
+        // Counting duration of sort
+        // Seeking the time
+        auto start = chrono::high_resolution_clock::now();
+        ShutterSort(A, K);
+
+        // Seeking the time again 
+        auto end = chrono::high_resolution_clock::now();
+
+        // Counting period of time
+        chrono::duration<double> duration = end - start;
+
+        // Filling in duration array
+        dur_arr[k] = duration.count();
+
+        printf("\n Duration: %.7f\n", dur_arr[k]);
+
+        sort(dur_arr, dur_arr + N);
+        for (int i = 100; i < N - 100; ++i)
+            aver_dur += dur_arr[i];
+        cout << "Average duration = " << aver_dur / (N - 201) << "s";
+    }
+}
+
