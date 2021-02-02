3f2095e1a61aa104f379edb34c9af7a4c7a5eaca
Daelon02 <gubin0202@gmail.com>
2021-02-02 09:25:24 +0200

    Added Lab11

---
+++ Lab11/Lab11/Lab11.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int sum(int a, int b)
+{
+    int res = a;
+
+    for (int i = 1; i <= b; ++i)
+    {
+        ++res;
+    }
+
+    return res;
+}
+
+int main()
+{
+    int a, b;
+
+    std::cout << "Введите a: ";
+    std::cin >> a;
+
+    std::cout << "Введите b: ";
+    std::cin >> b;
+
+    std::cout << "sum = " << sum(a, b) << "\n";
+
+    return 0;
+}
