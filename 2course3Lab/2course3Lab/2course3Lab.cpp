fe54af782432be8fcfcf746a7e9d15306bbc1eb2
Nikita <63471940+Daelon02@users.noreply.github.com>
2021-03-11 22:13:44 +0200

    Added 2couse3Lab

---
+++ 2course3Lab/2course3Lab/2course3Lab.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <time.h>
+#include <clocale>
+using namespace std;
+
+int main() {
+    srand(time(NULL));
+    const size_t n = 10;
+
+    int r, i, min, max, a[n];
+
+    cout << " Массив: ";
+    for (size_t i = 0; i < n; ++i)
+    {
+        a[i] = rand() % 100;
+        cout << setw(4) << a[i];
+    }
+    max = min = a[0];
+
+    for (i = 1; i < n; i++)
+    {
+        if (max < a[i]) max = a[i];
+        if (min > a[i]) min = a[i];
+    }
+    cout << "\n\n  max=" << max << endl;
+    cout << "\n  min=" << min << endl << endl;
+
+    r = max - min;
+
+    cout << "Массив без min и max: " << endl;
+    for (i = 0; i < n; i++) {
+        if (a[i] != min && a[i] != max) {
+            cout << a[i] << " ";
+        }
+    }
+}
---
+++ 2course3Lab/2course3Lab_2/2course3Lab_2.cpp
@@ -0,0 +1,73 @@
+﻿#include<iostream>
+#include<ctime>
+#include<Windows.h>
+#define N 100
+using namespace std;
+extern int* A, * B, * C, * D, sizes[5];
+int a[N], * A = a, b[N], * B = b, c[N], * C = c, d[N], * D = d, sizes[5];
+
+#include<iostream>
+#include<ctime>
+using namespace std;
+extern int* A, * B, * C, * D, sizes[5];
+void filling(int* z, const int size) {
+	srand(time(0));
+	for (int i = 0; i < size; ++i)
+		z[i] = rand() % 50 - 25;
+}
+void filling_arrs(int* w) {
+	const int A_size = sizes[0];
+	for (int i = 0; i < A_size; ++i) {
+		if (w[i] < 0) {
+			B[sizes[1]] = w[i];
+			sizes[1]++;
+		}
+		else if (w[i] > 0) {
+			C[sizes[2]] = w[i];
+			sizes[2]++;
+		}
+		else {
+			D[sizes[3]] = w[i];
+			sizes[3]++;
+		}
+	}
+
+}
+void output(int* z, int x) {
+	const int size = x;
+	for (int i = 0; i < size; ++i)
+		cout << z[i] << " ";
+	cout << endl;
+}
+void refilling(int* y, int* z, int x) {
+	const int size = x;
+	for (int i = 0; i < size; ++i)
+		y[i] = z[i];
+}
+
+int main() {
+	srand(time(0));
+	const int size = rand() % 10 + 1;
+	cout << "Размер массива: " << size << endl;
+	filling(A, size);
+	sizes[0] = size;
+	for (int i = 0; i < size; ++i)
+		cout << A[i] << " ";
+	cout << '\n';
+	filling_arrs(A);
+	cout << "Массив В" << endl << "> ";
+	output(B, sizes[1]);
+	cout << "Массив C" << endl << "> ";
+	output(C, sizes[2]);
+	cout << "Массив D" << endl << "> ";
+	output(D, sizes[3]);
+	cout << "Массив А с нулевыми элементами: " << '\n';
+	refilling(A, D, sizes[3]);
+	output(A, sizes[3]);
+	cout << "Массив А с отрицательными элементами: " << '\n';
+	refilling(A, B, sizes[1]);
+	output(A, sizes[1]);
+	cout << "Массив А с положительными элементами: " << '\n';
+	refilling(A, C, sizes[2]);
+	output(A, sizes[2]);
+}
